43a26f0169e84654e751c2155eed65075efa05e6
Erdem Bedel <erdem.bedel@yahoo.de>
2024-04-05 21:49:07 +0200

    Fix openApi generator in docker and add description for mailController

--- ep-backend/src/main/java/org/berlin/epbackend/controller/MailController.java
+++ ep-backend/src/main/java/org/berlin/epbackend/controller/MailController.java
@@ -1,15 +1,16 @@
 package org.berlin.epbackend.controller;
 
-import jakarta.mail.MessagingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.berlin.epbackend.dto.MailDTO;
 import org.berlin.epbackend.service.MailService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/mail")
@@ -22,8 +23,15 @@ public class MailController {
         this.mailService = mailService;
     }
 
+    @Operation(summary = "Send Email via Contact Form", description = "Endpoint to send an email through a contact form.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email sent successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - invalid details provided"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/send")
-    public void sendMail(@RequestBody MailDTO mailDTO) throws MessagingException, UnsupportedEncodingException {
+    public ResponseEntity<Void> sendMail(@RequestBody MailDTO mailDTO) {
         mailService.sendEmail(mailDTO);
+        return ResponseEntity.ok().build();
     }
 }
